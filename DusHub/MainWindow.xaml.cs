d89a20b2e8cb3f466ae510358c270bc326d0e473
chacoff <jaime.arctico@gmail.com>
2022-04-02 12:03:53 +0200

    Add project files.

---
+++ DusHub/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DusHub
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            FillGrid();
+        }
+
+        public class Data2Grid
+        {
+            public string? Name { get; set; }
+
+            public string? LastWriteTime { get; set; }
+        }
+
+        public void FillGrid()
+        {
+            var appSet = ConfigurationManager.AppSettings;
+            string Luf = appSet["LufApp"].ToString();
+            string[] splitLuf = Luf.Split('/');
+            List<Data2Grid> paths = new List<Data2Grid>();
+            DirectoryInfo[]? fileDir;
+
+            if (Directory.Exists(Luf))
+            {
+                fileDir = new DirectoryInfo(Luf).GetDirectories();
+                    if (splitLuf.Length > 3)
+                    {
+                        txtLuf.Content = splitLuf[0] + "/ ... /" + splitLuf[^2] + "/" + splitLuf[^1];
+                    }
+                    else
+                    {
+                        txtLuf.Content = Luf;
+                    }
+            }
+            else
+            {
+                fileDir = new DirectoryInfo(@"c:\").GetDirectories();
+                txtLuf.Content = @"C:\";
+            }
+            
+            foreach (var fi in fileDir)
+            {
+                paths.Add(new Data2Grid { Name = fi.Name, LastWriteTime = fi.LastWriteTime.ToString()});
+            }
+
+            MainGrid.ItemsSource = paths;
+        }
+
+        private void Grid_RightClick(object sender, RoutedEventArgs e)
+        {
+            //
+            var cellInfo = MainGrid.SelectedCells[0];
+            var cell = cellInfo.Column.GetCellContent(cellInfo.Item) as TextBlock;
+            Debug.WriteLine(cell.Text);
+        }
+
+        private void params_Click(object sender, RoutedEventArgs e)
+        {
+            Parameters win2 = new(this);
+            win2.ShowDialog();
+        }
+
+        private void Rip_Checked(object sender, RoutedEventArgs e)
+        {
+            var appSet = ConfigurationManager.AppSettings;
+
+            Rip.Content = "Ripscan (uncheck for Chop)";
+
+            RegistryKey localKey = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.CurrentUser, RegistryView.Registry64); // RegistryView.Registry32
+            localKey = localKey.OpenSubKey(appSet["RegeditApp"], true);
+            
+            if (localKey != null)
+            {
+                localKey.SetValue(appSet["KeyValueApp"], appSet["RipValueApp"]);
+                localKey.SetValue(appSet["KeyYieldApp"], appSet["RipYieldApp"]);
+            }
+
+            localKey.Close();
+        }
+
+        private void Rip_Unchecked(object sender, RoutedEventArgs e)
+        {
+            var appSet = ConfigurationManager.AppSettings;
+
+            Rip.Content = "Ripscan";
+
+            RegistryKey localKey = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.CurrentUser, RegistryView.Registry64); // RegistryView.Registry32
+            localKey = localKey.OpenSubKey(appSet["RegeditApp"], true);
+
+            if (localKey != null)
+            {
+                localKey.SetValue(appSet["KeyValueApp"], appSet["ChopValueApp"]);
+                localKey.SetValue(appSet["KeyYieldApp"], appSet["ChopYieldApp"]);
+            }
+
+            localKey.Close();
+        }
+    }
+}
---
+++ DusHub/Parameters.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Windows;
+using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DusHub
+{
+    /// <summary>
+    /// Interaction logic for Parameters.xaml
+    /// </summary>
+    public partial class Parameters : Window
+    {
+        MainWindow parent;
+
+        public Parameters(MainWindow parent)
+        {  
+            InitializeComponent();
+            Load_Parameters();
+            this.parent = parent;
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None); // @"DusHub.dll"
+
+            config.AppSettings.Settings["LufApp"].Value = txtLufBox.Text;
+            config.AppSettings.Settings["BatApp"].Value = txtTarget.Text;
+            config.AppSettings.Settings["RegeditApp"].Value = txtRegedit.Text;
+            config.AppSettings.Settings["KeyValueApp"].Value = txtRegeditKey.Text;
+            config.AppSettings.Settings["RipValueApp"].Value = txtRipValue.Text;
+            config.AppSettings.Settings["ChopValueApp"].Value = txtChopValue.Text;
+            config.AppSettings.Settings["KeyYieldApp"].Value = txtYieldKey.Text;
+            config.AppSettings.Settings["RipYieldApp"].Value = txtRipYield.Text;
+            config.AppSettings.Settings["ChopYieldApp"].Value = txtChopYield.Text;
+
+            config.Save(ConfigurationSaveMode.Modified, true);
+
+            ConfigurationManager.RefreshSection("appSettings");
+            parent.FillGrid();
+            this.Close();
+
+            // Process.Start(Application.ResourceAssembly.Location);
+            // Application.Current.Shutdown();
+        }
+
+        void Load_Parameters()
+        {
+            var appSet = ConfigurationManager.AppSettings;
+
+            txtLufBox.Text = appSet["LufApp"].ToString();
+            txtTarget.Text = appSet["BatApp"].ToString();
+            txtRegedit.Text = appSet["RegeditApp"].ToString();
+            txtRegeditKey.Text = appSet["KeyValueApp"].ToString();
+            txtRipValue.Text = appSet["RipValueApp"].ToString();
+            txtChopValue.Text = appSet["ChopValueApp"].ToString();
+            txtYieldKey.Text = appSet["KeyYieldApp"].ToString();
+            txtRipYield.Text = appSet["RipYieldApp"].ToString();
+            txtChopYield.Text = appSet["ChopYieldApp"].ToString();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Visibility = Visibility.Hidden;
+        }
+    }
+}
