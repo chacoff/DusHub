55d36c247ce7a8684e8fd59b84fdc4cf989916c3
chacoff <jaime.arctico@gmail.com>
2024-06-12 22:47:39 +0200

    first version with full dark theme

--- DusHub/MainWindow.xaml.cs
+++ DusHub/MainWindow.xaml.cs
@@ -42,6 +42,11 @@ namespace DusHub
 
         }
 
+        public void CloseWindow_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
         public void FillGrid()
         {
             var appSet = ConfigurationManager.AppSettings;
@@ -176,40 +181,6 @@ namespace DusHub
         private void ThemeToggleButton_Click(object sender, RoutedEventArgs e)
         {
             SwitchTheme();
-        }
-
-        public ICommand CloseCommand
-        {
-            get { return new RelayCommand(o => this.Close()); }
-        }
-
-    }
-
-    public class RelayCommand : ICommand
-    {
-        private Action<object> execute;
-        private Func<object, bool> canExecute;
-
-        public event EventHandler CanExecuteChanged
-        {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
-        }
-
-        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
-        {
-            this.execute = execute;
-            this.canExecute = canExecute;
-        }
-
-        public bool CanExecute(object parameter)
-        {
-            return this.canExecute == null || this.canExecute(parameter);
-        }
-
-        public void Execute(object parameter)
-        {
-            this.execute(parameter);
         }
     }
 }
